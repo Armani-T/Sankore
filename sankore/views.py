5de54cb25c9fafc4a3265c6b119fe9c99ce053ee
Armani Tallam <armanitallam@gmail.com>
2022-07-22 20:04:17 +0300

    Flesh out the start page with the main widgets that it will have.

--- sankore/views.py
+++ sankore/views.py
@@ -3,24 +3,36 @@ from PySide6 import QtWidgets as widgets
 APP_NAME = "Sankore"
 
 
-class StartPage(widgets.QWidget):
+class StartPage(widgets.QMainWindow):
     def __init__(self) -> None:
         super().__init__()
-        self.setWindowTitle(APP_NAME)
+        self.central_widget = widgets.QWidget()
 
-        layout = widgets.QHBoxLayout(self)
-        self.setLayout(layout)
+        layout = widgets.QHBoxLayout()
+        library_combo = widgets.QComboBox()
+        library_combo.addItem("Currently Reading")
+        library_table = widgets.QTableView()
+        library_table.resizeColumnsToContents()
+        update_button = widgets.QPushButton("Update reading position")
 
         left_layout = widgets.QVBoxLayout()
+        left_layout.addWidget(library_combo)
+        left_layout.addWidget(library_table)
         left_layout.addStretch()
         layout.addLayout(left_layout)
 
         right_layout = widgets.QVBoxLayout()
+        right_layout.addWidget(update_button)
+        right_layout.addWidget(widgets.QLabel("My Monthly Progress"))
         right_layout.addStretch()
         layout.addLayout(right_layout)
 
+        self.setWindowTitle(APP_NAME)
+        self.setCentralWidget(self.central_widget)
+        self.central_widget.setLayout(layout)
 
-def run_ui() -> int:
+
+def run_ui():
     """
     Construct and run the app UI.
 
@@ -32,5 +44,4 @@ def run_ui() -> int:
     app = widgets.QApplication()
     window = StartPage()
     window.show()
-    exit_code = app.exec()
-    return exit_code
+    app.exec()
