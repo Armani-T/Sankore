d110c5d4b6ed4b72ec220fd2462509ca823a7361
Armani Tallam <armanitallam@gmail.com>
2022-07-22 14:06:03 +0300

    Add the skeleton for the GUI

--- sankore/views.py
+++ sankore/views.py
@@ -1,5 +1,24 @@
 from PySide6 import QtWidgets as widgets
 
+APP_NAME = "Sankore"
+
+
+class StartPage(widgets.QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        self.setWindowTitle(APP_NAME)
+
+        layout = widgets.QHBoxLayout(self)
+        self.setLayout(layout)
+
+        left_layout = widgets.QVBoxLayout()
+        left_layout.addStretch()
+        layout.addLayout(left_layout)
+
+        right_layout = widgets.QVBoxLayout()
+        right_layout.addStretch()
+        layout.addLayout(right_layout)
+
 
 def run_ui() -> int:
     """
