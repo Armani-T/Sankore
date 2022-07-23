a575b1636601018edde7b5e6a51858108450b5fd
Armani Tallam <armanitallam@gmail.com>
2022-07-23 12:15:07 +0300

    Create a top level holder for all the pages in the UI
    I removed the start page temporarily. It should be back in the next
    commit.

--- sankore/views.py
+++ sankore/views.py
@@ -3,45 +3,20 @@ from PySide6 import QtWidgets as widgets
 APP_NAME = "Sankore"
 
 
-class StartPage(widgets.QMainWindow):
-    def __init__(self) -> None:
-        super().__init__()
-        self.central_widget = widgets.QWidget()
+class AppWindow(widgets.QMainWindow):
+    """The main window which holds all the pages in the app."""
 
-        layout = widgets.QHBoxLayout()
-        library_combo = widgets.QComboBox()
-        library_combo.addItem("Currently Reading")
-        library_table = widgets.QTableView()
-        library_table.resizeColumnsToContents()
-        update_button = widgets.QPushButton("Update reading position")
-
-        left_layout = widgets.QVBoxLayout()
-        left_layout.addWidget(library_combo)
-        left_layout.addWidget(library_table)
-        left_layout.addStretch()
-        layout.addLayout(left_layout)
-
-        right_layout = widgets.QVBoxLayout()
-        right_layout.addWidget(update_button)
-        right_layout.addWidget(widgets.QLabel("My Monthly Progress"))
-        right_layout.addStretch()
-        layout.addLayout(right_layout)
-
-        self.setWindowTitle(APP_NAME)
-        self.setCentralWidget(self.central_widget)
-        self.central_widget.setLayout(layout)
-
-
-def run_ui():
-    """
-    Construct and run the app UI.
-
-    Returns
-    -------
-    int
-        The exit code for the GUI. 0 means it stopped with no errors.
-    """
     app = widgets.QApplication()
-    window = StartPage()
-    window.show()
-    app.exec()
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle(APP_NAME)
+        self.to_start_page()
+
+    def run(self):
+        self.show()
+        self.app.exec()
+
+    def to_start_page(self):
+        start_page = StartPage()
+        self.setCentralWidget(start_page)
