41e60ed696acaf8fbce0051ab397b204bb1b7063
Armani <armanitallam@gmail.com>
2022-08-03 16:35:11 +0300

    Make all the getters and setters take the DB as an arg

--- sankore/models.py
+++ sankore/models.py
@@ -2,7 +2,7 @@ from collections.abc import Collection
 from dataclasses import dataclass
 from itertools import chain
 from pathlib import Path
-from shelve import open as shelve_open
+from shelve import open as open_shelf
 from typing import Mapping, MutableMapping
 
 
@@ -47,42 +47,39 @@ DEFAULT_LIBRARIES: Mapping[str, str] = {
 
 
 def get_db(db_file: Path) -> Database:
-    open_flag = "w" if db_file.exists() else "n"
-    db = shelve_open(db_file, flag=open_flag, protocol=4)
+    db = open_shelf(db_file, flag="c", protocol=4)
     for library in DEFAULT_LIBRARIES:
         if library not in db:
             db[library] = Library(DEFAULT_LIBRARIES[library], ())
     return db
 
 
-def create_book(lib_name: str, new_book: Book) -> None:
-    LIBRARIES[lib_name] = (*get_books(lib_name), new_book)
+def create_book(db: Database, name: str, new_book: Book) -> None:
+    db[name] = (*get_books(name), new_book)
 
 
-def change_library(old_lib: str, new_lib: str, book: Book) -> None:
-    LIBRARIES[old_lib] = tuple(item for item in get_books(old_lib) if item != book)
+def change_library(db: Database, old_lib: str, new_lib: str, book: Book) -> None:
+    db[old_lib] = tuple(item for item in get_books(old_lib) if item != book)
     create_book(new_lib, book)
 
 
-def get_book(title: str) -> Book:
-    for book in get_books(ALL_BOOKS):
+def get_book(db: Database, title: str) -> Book:
+    for book in get_books(db):
         if book.title == title:
             return book
     raise ValueError(f'No book with title "{title}" was found.')
 
 
-def get_books(name) -> Library:
-    return sum(LIBRARIES.values(), Library()) if name == ALL_BOOKS else LIBRARIES[name]
+def get_books(db: Database, name: str) -> Library:
+    return sum(db.values(), Library()) if name == ALL_BOOKS else db[name]
 
 
-def get_libraries(all_: bool = True) -> Collection[str]:
-    names = list(LIBRARIES.keys())
+def get_libraries(db: Database, all_: bool = True) -> Collection[str]:
+    names = list(db.keys())
     if all_:
         names.insert(0, ALL_BOOKS)
     return names
 
 
-def update_book(lib_name: str, old_title: str, new_book: Book) -> None:
-    LIBRARIES[lib_name] = tuple(
-        new_book if book.title == old_title else book for book in LIBRARIES[lib_name]
-    )
+def update_book(db: Database, name: str, old_title: str, new_book: Book) -> None:
+    db[name] = tuple(new_book if book.title == old_title else book for book in db[name])
