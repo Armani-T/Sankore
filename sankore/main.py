12172596e01a98e7d8735ed874e004642f1c88b6
Armani <armanitallam@gmail.com>
2022-08-08 10:14:24 +0300

    Set the database file path
    Ignore it in the `.gitignore` file because everyone has their own local
    file.

--- sankore/main.py
+++ sankore/main.py
@@ -1,12 +1,16 @@
+from pathlib import Path
 from typing import NoReturn
 
+from models import get_db
 from views import run_ui
 
 APP_NAME = "Sankore"
+DB_FILE = Path(__file__).joinpath("../../data/libraries.dbm").resolve(strict=True)
 
 
 def main() -> NoReturn:
-    exit_code = run_ui(APP_NAME)
+    db = get_db(str(DB_FILE))
+    exit_code = run_ui(APP_NAME, db)
     exit(exit_code)
 
 
