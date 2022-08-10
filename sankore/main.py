3567189ed06e7c85d996d80f41de889857fa5c84
Armani <armanitallam@gmail.com>
2022-08-10 14:17:06 +0300

    Update the `main` function

--- sankore/main.py
+++ sankore/main.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from typing import NoReturn
 
-from models import get_db
+from models import get_data
 from views import run_ui
 
 APP_NAME = "Sankore"
@@ -9,7 +9,8 @@ DATA_FILE = Path(__file__).joinpath("../../data.json").resolve(strict=True)
 
 
 def main() -> NoReturn:
-    data = get_data(DATA_FILE)
+    DATA_FILE.touch(exist_ok=True)
+    data = get_data(str(DATA_FILE.resolve(strict=True)))
     exit_code = run_ui(APP_NAME, data)
     exit(exit_code)
 
