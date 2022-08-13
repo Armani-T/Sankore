ea2bf1b3689181ea3b29c00ea4c65590c1e1377b
Armani <armanitallam@gmail.com>
2022-08-13 08:39:42 +0300

    Refactor the `model` lens functions

--- sankore/models.py
+++ sankore/models.py
@@ -1,14 +1,11 @@
-from collections.abc import Collection, Iterable
-from dataclasses import dataclass
 from itertools import chain
 from json import loads
 from pathlib import Path
-from shelve import Shelf
-from typing import TypedDict
+from typing import Iterable, TypedDict
 
 ALL_BOOKS = "All Books"
 
-Data = dict[str, Library]
+Data = dict[str, "Library"]
 
 
 class Book(TypedDict):
@@ -29,43 +26,35 @@ def get_data(data_file: Path) -> Data:
     return json["libraries"]
 
 
-def insert_book(db: Database, library: str, new_book: Book) -> None:
-    db[library] = db[library].insert(new_book)
+def insert_book(data: Data, library: str, new_book: Book) -> None:
+    data[library]["books"].append(new_book)
 
 
-def find_book(db: Database, book_title: str) -> Book:
-    full_list = list_all_books(db)
-    return full_list[book_title]
+def find_book(data: Data, book_title: str) -> Book:
+    for book in list_all_books(data):
+        if book["title"] == book_title:
+            return book
+    raise KeyError(f"No book with the title: {book_title}")
 
 
-def list_all_books(db: Database) -> Library:
-    result = sum(db.values(), Library())
-    result.description = "All the books recorded in the library."
-    return result
+def list_all_books(data: Data) -> Iterable[Book]:
+    get_books = lambda library: library["books"]
+    book_lists = map(get_books, data.values())
+    return chain(book_lists)
 
 
-def list_books(db: Database, name: str) -> Library:
-    return list_all_books(db) if name == ALL_BOOKS else db[name]
+def list_books(data: Data, name: str) -> Iterable[Book]:
+    if name == ALL_BOOKS:
+        return list_all_books(data)
+    return data[name]["books"]
 
 
-def list_libraries(db: Database, all_: bool = True) -> Iterable[str]:
+def list_libraries(data: Data, all_: bool = True) -> Iterable[str]:
     if all_:
         yield ALL_BOOKS
-    yield from db.keys()
+    yield from data.keys()
 
 
-def switch_library(db: Database, old_name: str, new_name: str, book: Book) -> None:
-    old_lib = db[old_name]
-    db[old_lib] = Library(
-        old_lib.description,
-        [item for item in old_lib.books if item != book],
-    )
-    return insert_book(db, new_name, book)
-
-
-def update_book(db: Database, name: str, old_title: str, new_book: Book) -> None:
-    library = db[name]
-    db[name] = Library(
-        library.description,
-        [new_book if book.title == old_title else book for book in db[name]],
-    )
+def update_book(data: Data, name: str, old_title: str, new_book: Book) -> None:
+    replace = lambda book: new_book if book.title == old_title else book
+    data[name]["books"] = list(map(replace, data[name]["books"]))
