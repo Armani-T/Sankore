7ba8337961220c542777dfbf59bcf3451c40cdd7
Armani <armanitallam@gmail.com>
2022-11-19 13:42:47 +0300

    Update the `main` module to work with the new `models` API

--- sankore/main.py
+++ sankore/main.py
@@ -1,27 +1,18 @@
 #!/usr/bin/env python3
 from pathlib import Path
 from typing import NoReturn
-import json
 
-from models import Book
+from models import get_cursor
 from views import run_ui
 
-APP_NAME = "Sankore"
-DATA_FILE = Path(__file__).joinpath("../../data.json").resolve()
+APP_NAME = "sankore"  # NOTE: The app name should always be in lowercase.
+DB_FILE = Path(__file__).joinpath(f"../../{APP_NAME}.sqlite3").resolve()
 
 
 def main() -> NoReturn:
-    try:
-        DATA_FILE.touch(exist_ok=True)
-        json_data = json.load(DATA_FILE.open())
-        data = [Book(**obj) for obj in json_data.get("books", ())]
-        updated_data, exit_code = run_ui(APP_NAME, data)
-    except (FileNotFoundError, json.decoder.JSONDecodeError):
-        exit(1)
-    else:
-        string = json.dumps({"books": [book.to_dict() for book in updated_data]})
-        DATA_FILE.write_text(string, "utf8")
-        exit(exit_code)
+    cursor = get_cursor(DB_FILE)
+    exit_code = run_ui(APP_NAME.title(), cursor)
+    exit(exit_code)
 
 
 if __name__ == "__main__":
